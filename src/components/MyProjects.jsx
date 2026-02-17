80b8b439da13348a8b90b5ae82071c1f254a0484
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-02-17 14:10:32 -0300

    FEAT: add icons aos projetos

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
+import { TbBrandDjango,TbBrandJavascript,TbBrandHtml5,TbBrandCss3,TbBrandTailwind,TbBrandReact } from "react-icons/tb";
+
+
+
 
 function MyProjects() {
   const [filtro, setFiltro] = useState('Todos')
+  const Django = <TbBrandDjango className='text-[#092E20]'/>;
+  const Javascript = <TbBrandJavascript className='text-yellow-400'/>
+  const Html = <TbBrandHtml5  className ='text-orange-400'/>
+  const Css = <TbBrandCss3 className='text-blue-400'/>
+  const Tailwind = <TbBrandTailwind  className='text-blue-400'/>
+  const React = <TbBrandReact  className='text-purple-600'/>
   const projetos = [
     {
       id: 1,
       nome: 'Gerenciador de tarefas',
       tipo: 'React',
+      tecs: <>{React} {Tailwind} </>  ,
       descricao:
         'Website para organizar tarefas diárias, controlar prazos e aumentar a produtividade pessoal e profissional. ',
       link: 'curso-react-ashy-xi.vercel.app',
@@ -15,17 +26,19 @@ function MyProjects() {
       id: 2,
       nome: 'NutriVitta',
       tipo: 'Html+Css+Js',
+      tecs: <> {Javascript} {Html} {Css} </>  ,
       descricao:
         'Site educativo sobre alimentação equilibrada, com dicas, calculadora de IMC, quizzes e cardápios personalizados. ',
       link: 'new-pi-2k25.vercel.app',
     },
     {
       id: 6,
-      nome: 'Calculadora',
-      tipo: 'Html+Css+Js',
+      nome: 'sistema de gestão clínica',
+      tipo: 'Django',
+      tecs: <>{Django}</>,
       descricao:
-        'Ferramenta prática para realizar cálculos matemáticos básicos de forma rápida e intuitiva. ',
-      link: 'otavioaugustodaroza.github.io/calculadora/',
+        'Sistema de gestão clínica SIMPLES do qual tem foco em aprendizagem de tecnologias backend',
+      link: 'https://github.com/OtavioAugustodaRoza/sistema_gestao_clinica.git',
     },
     {
       id: 3,
@@ -36,6 +49,7 @@ function MyProjects() {
           </span>
         </>
       ),
+       tecs: <>{React} {Tailwind} </> ,
       descricao:
         'Projeto de rede social inclusiva para pessoas neurodivergentes, oferecendo um ambiente seguro. ',
       link: 'ainda não temos',
@@ -45,16 +59,22 @@ function MyProjects() {
       id: 4,
       nome: 'Este Site',
       tipo: 'React',
+      tecs: <>{React} {Tailwind} </> ,
       descricao:
         'Website pessoal para exibir projetos, experiências e portfólio de forma organizada e atrativa para todos. ',
       link: 'https://my-portifolio-iota-inky.vercel.app/',
     },
     {
       id: 5,
-      nome: 'Detox digital',
-      tipo: 'Html+Css+Js',
+      nome:   <>
+      <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 bg-clip-text text-transparent font-bold">
+        (Em desenvolvimento)
+      </span>
+    </>,
+      tipo: 'React',
+      tecs: <>{React} {Tailwind}</> ,
       descricao:
-        'Website voltado para ajudar a reduzir o tempo de tela e incentivar hábitos digitais mais saudáveis. ',
+        'AudioBook completo com opção de postagem de músicas e criação de playlists com intuito em desenvolver uma comunidade . ',
       link: 'rafagameplays.github.io/site-de-telas-finalizado/questionario.html',
     },
   ]
@@ -77,9 +97,10 @@ function MyProjects() {
           Confira meus projetos mais recentes
         </h2>
       </div>
+      
 
       <div className="flex gap-4 rounded-2xl border-2 border-purple-600 p-1 text-purple-600">
-        {['Todos', 'React', 'Html+Css+Js'].map((item) => (
+        {['Todos', 'React', 'Html+Css+Js', 'Django'].map((item) => (
           <button
             key={item}
             onClick={() => setFiltro(item)}
@@ -103,7 +124,7 @@ function MyProjects() {
             <div className="flex flex-col gap-4 justify-center items-center">
               <div className="flex flex-col gap-2 justify-center items-center">
                 <h3 className="text-2xl font-bold">{pro.nome}</h3>
-                <h4 className="font-semibold text-blue-600">{pro.tipo}</h4>
+                <h4 className="text-4xl flex">{pro.tecs}</h4>
               </div>
               <p className="text-gray-600 text-[20px]">{pro.descricao}</p>
               <a
